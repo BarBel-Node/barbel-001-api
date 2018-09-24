5bbbf4ff2d8589aa2c9c36b4875234fcca5ef8d8
Firmansyah Adiputra <frm.adiputra@gmail.com>
2018-09-24 22:48:45 +0700

    Store endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = 3000
@@ -10,12 +11,19 @@ app.use(cors({
 	]
 }))
 
+// Parser for request body in JSON format
+const jsonParser = bodyParser.json()
+
 // Routes and handler
 
 app.get('/hello', (req, res) => res.json({msg: 'Hello World!'}))
 
 app.get('/greetings', (req, res) => {
 	res.json({msg: 'Hi ' + req.query.name + '!'})
+})
+
+app.post('/store', jsonParser, (req, res) => {
+	res.json({msg: `Storing ${req.body.name}=${req.body.value}`})
 })
 
 // Start the server
